81ffa0c3942cb0f3697c7484365764ce3ad928b6
elenaburdin <66010914+elenaburdin@users.noreply.github.com>
2023-03-09 10:02:20 +0100

    Add files via upload

---
+++ Word Blanks/main.js
@@ -0,0 +1,9 @@
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = "When I'm at the kitchen, my " + myNoun + " hear the sound of the oppened " + myAdjective + " chips, and " + myVerb + " very " + myAdverb + "."; // Change this line
+// Only change code above this line
+console.log(wordBlanks);
