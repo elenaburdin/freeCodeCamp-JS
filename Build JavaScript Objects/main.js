882f8acdd139cae4af8d7e26cc8a0358c4d514d4
elenaburdin <66010914+elenaburdin@users.noreply.github.com>
2023-03-12 18:07:40 +0100

    Add files via upload

---
+++ Build JavaScript Objects/main.js
@@ -0,0 +1,12 @@
+const myDog = {
+  name: 'William',
+  legs: 4,
+  tails: 1,
+  friends: ['everyone']
+};
+
+// Tests
+console.log(myDog.name);
+console.log(myDog.legs);
+console.log(myDog.tails);
+console.log(myDog.friends);
