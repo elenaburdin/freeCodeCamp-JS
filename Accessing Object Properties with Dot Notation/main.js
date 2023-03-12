ee534ed68fb6289706a62986b32172fbcb41df1e
elenaburdin <66010914+elenaburdin@users.noreply.github.com>
2023-03-12 18:11:02 +0100

    Add files via upload

---
+++ Accessing Object Properties with Dot Notation/main.js
@@ -0,0 +1,13 @@
+// Setup
+const testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+const hatValue = testObj.hat;
+const shirtValue = testObj.shirt;
+
+// Tests
+console.log(hatValue);
+console.log(shirtValue);
