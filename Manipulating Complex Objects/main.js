eebf5b1ac102b7db6395d3d6701bfa2452e8d7e9
elenaburdin <66010914+elenaburdin@users.noreply.github.com>
2023-03-12 19:56:52 +0100

    Add files via upload

---
+++ Manipulating Complex Objects/main.js
@@ -0,0 +1,23 @@
+const myMusic = [
+    {
+        "artist": "Billy Joel",
+        "title": "Piano Man",
+        "release_year": 1973,
+        "formats": [
+            "CD",
+            "8T",
+            "LP"
+        ],
+        "gold": true
+    },
+    {
+        "artist": "Some Artist",
+        "title": "Random Article",
+        "release_year": 2023,
+        "formats": [
+            "CD",
+            "Podcast",
+            "SomeFormat"
+        ]
+    }
+];
